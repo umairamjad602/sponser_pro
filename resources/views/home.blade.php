c318e7c3de63c51537ee3d2cb346e1cce35d6b07
habiba zulfiqar <habibazulfiqar9@gmail.com>
2023-03-14 19:33:33 +0500

    Update CSS and blade changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,9 +20,10 @@
 
 
 <body>
-<div class="loader_wrap"><div class="loader">Loading...</div></div>
+
    @include('dashboard.modals.login-modal')
    @include('dashboard.modals.registration-modal')
+    <div class="loader_wrap"><div class="loader">Loading...</div></div> 
   
     <!-- header -->
     <div style="background-image: url({{ asset('front-end/images/Sponser\ Pro.png') }});"
@@ -528,7 +529,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
-   <script>
+   
+
+</body>
+	<script>
 		window.addEventListener('DOMContentLoaded', function() {
   setTimeout(function(){
   $('.loader_wrap').fadeOut(800);
@@ -536,7 +540,5 @@
   },2000);
 })
 	</script>
-
-</body>
 
 </html>

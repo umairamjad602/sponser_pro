dddfc14af112d7a1f9ecdeb961dd0e17a6afb297
habiba zulfiqar <habibazulfiqar9@gmail.com>
2023-03-16 19:07:52 +0500

    Update CSS and blade changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ asset('front-end/sponser-favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('front-end/assets/images/sponser-favicon.png') }}">
     <link rel="stylesheet" href="{{ asset('front-end/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('front-end/css/home.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
